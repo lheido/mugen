e5c42b4606d3ae08ae02be5b869d09b0c075cbc3
leonard <leonard@digicoop.io>
2024-06-08 11:28:24 +0200

    feat: determine the best content color according to the background.

--- packages/mugen/theme/src/surface/surface.css.ts
+++ packages/mugen/theme/src/surface/surface.css.ts
@@ -11,22 +11,89 @@ import {
   lightModifier,
 } from "../../colors.css";
 import { mugen } from "../../layers.css";
-import { HexColor, hexToRgb, hslToRgb, rgbToHex, rgbToHsl } from "../color";
+import {
+  HexColor,
+  RgbColor,
+  getContrastRatio,
+  hexToRgb,
+  hslToRgb,
+  rgbToHex,
+  rgbToHsl,
+} from "../color";
 import { ColorNames, FullSurfaceColors, SurfaceColors } from "./surface.types";
 
+function getBestContentColor(base: RgbColor, light: RgbColor, dark: RgbColor) {
+  const baseContentColors = [hexToRgb("#000000"), hexToRgb("#ffffff")]; // black, white
+  const bases = [base, light, dark];
+  let startColorRatio = 0;
+  let startColor!: RgbColor;
+  let selectedBaseColor!: RgbColor;
+
+  for (let color of baseContentColors) {
+    for (let base of bases) {
+      let tmp = getContrastRatio(color, base);
+      if (tmp > startColorRatio) {
+        startColorRatio = tmp;
+        startColor = color;
+        selectedBaseColor = base;
+      }
+    }
+  }
+  if (startColorRatio > 7) {
+    const startLuminance = rgbToHsl(startColor).l;
+    return _getBestContentColor(
+      selectedBaseColor,
+      startColor,
+      startColor,
+      startLuminance
+    );
+  } else {
+    return startColor;
+  }
+}
+
+function _getBestContentColor(
+  base: RgbColor,
+  startColor: RgbColor,
+  previousColor: RgbColor,
+  startLuminance: number
+) {
+  const ratio = getContrastRatio(base, startColor);
+  if (ratio <= 7) {
+    return previousColor;
+  }
+  const nextColor = rgbToHsl(startColor);
+  const baseHsl = rgbToHsl(base);
+  nextColor.h = baseHsl.h;
+  nextColor.s = baseHsl.s;
+  nextColor.l =
+    startLuminance === 100
+      ? Math.max(nextColor.l - 1, 0)
+      : Math.min(nextColor.l + 1, 100);
+  return _getBestContentColor(
+    base,
+    hslToRgb(nextColor),
+    startColor,
+    startLuminance
+  );
+}
+
 function createSurfaceVars(name: ColorNames, color: HexColor) {
-  const light = rgbToHsl(hexToRgb(color));
+  const baseRgb = hexToRgb(color);
+  const light = rgbToHsl(baseRgb);
   light.l = Math.min(light.l + lightModifier, 100);
-  const lightHex = rgbToHex(hslToRgb(light));
+  const ligthRgb = hslToRgb(light);
+  const lightHex = rgbToHex(ligthRgb);
   const dark = rgbToHsl(hexToRgb(color));
   dark.l = Math.max(dark.l - darkModifier, 0);
-  const darkHex = rgbToHex(hslToRgb(dark));
-  // TODO: calculate the content color according to the darkest and lightest color.
+  const darkRgb = hslToRgb(dark);
+  const darkHex = rgbToHex(darkRgb);
+  const content = rgbToHex(getBestContentColor(baseRgb, ligthRgb, darkRgb));
   return {
     [name]: color,
     [`${name}Light`]: lightHex,
     [`${name}Dark`]: darkHex,
-    [`${name}Content`]: "#000000",
+    [`${name}Content`]: content,
   } as FullSurfaceColors;
 }
 
