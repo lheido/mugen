300bfcd6eb4a04950857e30b138a33d13b1c17ba
leonard <leonard@digicoop.io>
2024-06-24 11:41:52 +0200

    feat: Create the muden documentation app using astro

---
+++ apps/mugen-doc/astro.config.mjs
@@ -0,0 +1,12 @@
+import { defineConfig } from "astro/config";
+
+import solidJs from "@astrojs/solid-js";
+import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [solidJs()],
+  vite: {
+    plugins: [vanillaExtractPlugin()],
+  },
+});
---
+++ apps/mugen-doc/src/layouts/index.css.ts
@@ -0,0 +1,27 @@
+import { base, surfaces } from "@mugen/theme/theme.css";
+import { globalStyle } from "@vanilla-extract/css";
+
+export { darkTheme } from "@mugen/theme/theme.css";
+
+globalStyle("body", {
+  "@layer": {
+    [base]: {
+      margin: 0,
+      fontFamily: `-apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+        'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+        sans-serif`,
+      WebkitFontSmoothing: "antialiased",
+      MozOsxFontSmoothing: "grayscale",
+      backgroundColor: `hsl(${surfaces.base})`,
+      color: `hsl(${surfaces["base-content"]})`,
+    },
+  },
+});
+
+globalStyle("code", {
+  "@layer": {
+    [base]: {
+      fontFamily: `source-code-pro, Menlo, Monaco, Consolas, 'Courier New', monospace`,
+    },
+  },
+});
