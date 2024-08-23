7605fc603b4ec0e4a416f003c7e3dcadc8239b08
leonard <leonard@digicoop.io>
2024-08-23 16:49:45 +0200

    chore: Refactor Mugen template initialization script

--- mugen-init.js
+++ mugen-init.js
@@ -2,41 +2,35 @@
  * This script is used to initialize the Mugen template after the repository has been created from the template.
  *
  * It will:
- * 1. Remove the apps content.
+ * 1. Clean git history.
  * 2. Install dependencies.
- * 3. Remove itself.
- * 4. Clean git history.
  */
 
 const fs = require("fs");
 const path = require("path");
 const { execSync } = require("child_process");
 
-const removeAppsContent = () => {
-  const appsDir = path.join(__dirname, "apps");
-  fs.readdirSync(appsDir).forEach((appDirectory) => {
-    const appPath = path.join(appsDir, appDirectory);
-    fs.rmSync(appPath, { recursive: true });
-  });
-};
-
 const installDependencies = () => {
   execSync("pnpm install");
 };
 
-const removeInitScript = () => {
-  fs.unlinkSync(__filename);
-};
-
 const cleanGitHistory = () => {
   execSync(
-    "git filter-branch --index-filter 'git rm --cached --ignore-unmatch ./mugen-init.js ./apps/**' HEAD"
+    "git filter-branch --index-filter 'git rm --cached --ignore-unmatch ./mugen-init.js ./pnpm-lock.yaml ./apps/**' HEAD"
   );
+  // Recreate the app directory and add a .gitkeep file
+  fs.mkdirSync(path.join(__dirname, "apps"));
+  fs.writeFileSync(path.join(__dirname, "apps/.gitkeep"), "");
 };
 
-// removeAppsContent();
+const updateRepository = () => {
+  execSync("git add .");
+  execSync("git commit -m 'Initialize Mugen template'");
+  execSync("git push --force origin master");
+};
+
 cleanGitHistory();
 installDependencies();
-// removeInitScript();
+updateRepository();
 
 console.log("Mugen template initialized successfully!");
